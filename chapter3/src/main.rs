fb2d8602b6fa2c3334909009b4e92c796f676b27
Gudsteinn Arnarson <gudsteinn.arnason@gmail.com>
2025-04-06 23:32:13 +0200

    half way with chapter 3

---
+++ chapter3/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    //Variables
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of X is {x}");
+    }
+
+    println!("The value of X is {x}.");
+
+    let _guess: u32 = "42".parse().expect("Not a number!");
+
+    //floating pints
+    //let x = 2.0; // f64
+
+    //let y: f32 = 3.0; // f32
+    
+    func();
+}
+
+fn func(){ println!("Hello there");}
